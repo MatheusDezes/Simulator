11fcdec12dc31665e6ce64cfffea8ce29a786b78
MatheusDezes <mateodezes@gmail.com>
2024-08-01 17:45:51 -0300

    DropDown Simulator

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const dropDowns = document.querySelectorAll('.dropdown');
+
+dropDowns.forEach(dropDowns => {
+    const select = dropDowns.querySelectorAll('.select');
+    const iconDrop = dropDowns.querySelectorAll('.icon-drop');
+    const menu = dropDowns.querySelectorAll('.menu');
+    const selected = dropDowns.querySelectorAll('.selected');
+})
+
