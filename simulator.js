7d50502bff52cbad0f625b33b768b95bac342916
MatheusDezes <mateodezes@gmail.com>
2024-08-15 14:05:45 -0300

    :Fix: Checkbox

---
+++ simulator.js
@@ -0,0 +1,7 @@
+var blackWork = 300;
+const valueSimulation = document.querySelectorAll('#valorSimulation');
+const btnSimulator = document.querySelectorAll('.btn-whatsapp');
+
+btnSimulator.addEventListener('click', (valueSimulation) => {
+    valueSimulation.textContent = "300";
+});
