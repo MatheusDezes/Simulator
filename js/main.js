a5d3e642e39beaa67565c419b4d5069f9ca35bf4
MatheusDezes <mateodezes@gmail.com>
2024-08-20 12:10:29 -0300

    Sistema de simulação completo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+
 // =======Dropdown=========
 //Seleção do Dropdown
 const dropDowns = document.querySelectorAll('.dropdown');
-//Todos os 
+
 dropDowns.forEach(dropDowns => {
     const select = dropDowns.querySelector('.select');
     const iconDrop = dropDowns.querySelector('.icon-drop');
@@ -27,7 +28,68 @@ dropDowns.forEach(dropDowns => {
                 select.classList.remove('select-clicked');
                 iconStyle.classList.remove('icon-style-clicked');
             });
+            
             option.classList.add('active');
+            
+            //### Simulador 
+
+            // Objeto com os valores dos estilos de Tatuagens
+            const tattooStyles = {
+                BlackWork: {
+                    flash: "R$320 R$750",
+                    fechamento: "R$830 R$1190"
+                },
+                Realismo: {
+                    flash: "R$490 R$990",
+                    fechamento: "R$990 R$1990"
+                },   
+                OldSchool: {
+                    flash: "R$330 R$730",
+                    fechamento: "R$850 R$1190"
+                },
+                FineLine: {
+                    flash: "R$310 R$750",
+                    fechamento: "R$840 R$1190"
+                }
+            };
+            // Somando o valor 
+            const selectedStyle = option.innerText.replace(' ', '');
+            const style = tattooStyles[selectedStyle];
+            
+            // Selecionando os elementos
+            const Button = document.querySelector('.btn-simular');
+            const valorSimulation = document.querySelector('#valorSimulation');
+            const checkboxFlash = document.getElementById('flashCheck');
+            const checkboxFechamento = document.getElementById('fechamentoCheck');
+            const nomeInput = document.getElementById('nome');
+                
+                // Adiciona um evento de clique ao botão "Simular"
+                Button.addEventListener('click', (event) => {
+                    // Impede o comportamento padrão do botão (atualizar a página)
+                    event.preventDefault();
+
+                    // Verifica se o campo de nome está vazio
+                    if (nomeInput.value.trim() === '') {
+                    alert('Por favor, preencha o campo de nome e telefone antes de simular.');
+                    return; // Impede que o código continue se o nome não estiver preenchido
+                    }
+
+                    let result = ''; // Variável para armazenar o resultado que será exibido
+                    
+                    // Verifica se o checkbox "Flash" está marcado
+                    if (checkboxFlash.checked) {
+                        result += style.flash;
+                    }
+                    
+                    // Verifica se o checkbox "Fechamento" está marcado
+                    if (checkboxFechamento.checked) {
+                        result += style.fechamento;
+                    }
+                
+                // Atualiza a interface com o resultado
+                valorSimulation.innerText = result;
+            
+            });
         });
     });
 });
@@ -37,13 +99,13 @@ dropDowns.forEach(dropDowns => {
 var maskPhone = document.getElementById('telefone');
 
 maskPhone.addEventListener("input", () => {
-
+    
     // Remover os caracteres não numéricos usando a expressão regular /\D/g e limitar a 11 digitos.
     var limparValor = maskPhone.value.replace(/\D/g, "").substring(0,11);
-
+    
     // Dividir a string em um array de caracteres individuais 
     var numerosArray = limparValor.split("");
-
+    
     // Criar a variável para receber o numero formatado
     var numeroFormatado = "";
 
@@ -84,33 +146,5 @@ checkboxes.forEach((checkbox) => {
 });
 
 
-//### Simulador 
 
-
-const button = document.querySelector('.btn-simular');
-const valorSimulation = document.querySelector('#valorSimulation');
-
-
-const tattooStyles = {
-    blackwork: {
-        flash: "R$320 R$750",
-        fechamento: "R$830 R$1190"
-    },
-    realismo: {
-        flash: "R$490 R$990",
-        fechamento: "R$990 R$1990"
-    },   
-    oldSchool: {
-        flash: "R$330 R$730",
-        fechamento: "R$850 R$1190"
-    },
-    fineLine: {
-        flash: "R$310 R$750",
-        fechamento: "R$840 R$1190"
-    }
-};
-
-button.addEventListener('click', () => {
-    valorSimulation.innerText = tattooStyles.blackwork.flash;
-});
 
